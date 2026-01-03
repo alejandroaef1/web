fea286b880543be52b4e08ed472978f98cdc68cb
Alex Antonio <alejandroaef@ciencias.unam.mx>
2026-01-03 15:59:23 -0600

    Actualizacion

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-// Archivo JavaScript opcional para funciones futuras
+const imagenes = [
+  'animacion_1/b1.png',
+  'animacion_1/b2.png',
+  'animacion_1/b3.png',
+  'animacion_1/b4.png',
+  'animacion_1/b5.png',
+  'animacion_1/b6.png',
+];
 
-console.log("Portafolio cargado correctamente.");
+let indice = 0;
+const img = document.getElementById('imagen');
+
+setInterval(() => {
+  indice = (indice + 1) % imagenes.length;
+  img.style.opacity = 0;
+
+  setTimeout(() => {
+    img.src = imagenes[indice];
+    img.style.opacity = 1;
+  }, 500);
+}, 2000);
+
